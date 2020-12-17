1d1a76efc774ade267f2303c10beb1d86b043fd7
“ynikitin” <ynikitin@computervoice.ro>
2020-12-17 14:17:04 +0200

    done lab3

--- app/src/main/java/com/example/androidlab25/MainActivity.kt
+++ app/src/main/java/com/example/androidlab25/MainActivity.kt
@@ -3,61 +3,68 @@ package com.example.androidlab25
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fromSpinner: Spinner
-    lateinit var toSpinner: Spinner
-    lateinit var fromCurrency : String
-    lateinit var toCurrency : String
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        fromSpinner = findViewById(R.id.from)
-        toSpinner = findViewById(R.id.to)
-
-        fromSpinner.onItemSelectedListener = object :
-
-                AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (adapterView != null) {
-                    fromCurrency = adapterView.getItemAtPosition(position).toString()
-                    println(fromCurrency)
-                }
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-
-            }
-
-        }
-        toSpinner.onItemSelectedListener = object :
-
-                AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (adapterView != null) {
-                    toCurrency = adapterView.getItemAtPosition(position).toString()
-                    println(toCurrency)
-                }
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-
-            }
-
-        }
     }
 
-    fun showSecondActivity(view: View) {
-        val intent = Intent(this, MainActivity2::class.java)
-        startActivity(intent)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        lateinit var fragment: Fragment
+        if (item?.itemId == R.id.share_menu) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, "You shared data")
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        } else if (item.itemId == R.id.about_menu) {
+            fragment = AboutFragment()
+            val fragmentManager = supportFragmentManager
+            val fragmentTransition = fragmentManager.beginTransaction()
+            fragmentTransition.replace(R.id.fr_place, fragment)
+            fragmentTransition.commit()
+            val mainFragment = BlankFragment1()
+            mainFragment.view?.visibility = View.GONE
+        } else if (item.itemId == R.id.main_page) {
+            fragment = BlankFragment1()
+            val fragmentManager = supportFragmentManager
+            val fragmentTransition = fragmentManager.beginTransaction()
+            fragmentTransition.replace(R.id.fr_place, fragment)
+            fragmentTransition?.commit()
+        } else if (item.itemId == R.id.terms_menu) {
+            fragment = TermsFragment()
+            val fragmentManager = supportFragmentManager
+            val fragmentTransition = fragmentManager.beginTransaction()
+            fragmentTransition.replace(R.id.fr_place, fragment)
+            fragmentTransition?.commit()
+        }else {
+            return super.onOptionsItemSelected(item)
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/androidlab25/TermsFragment.kt
+++ app/src/main/java/com/example/androidlab25/TermsFragment.kt
@@ -1,19 +1,18 @@
 package com.example.androidlab25
 
 import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class AboutFragment : Fragment() {
+class TermsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
-    }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        return inflater.inflate(R.menu.menu, menu)
+        return inflater.inflate(R.layout.fragment_terms, container, false)
     }
 }
